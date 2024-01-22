06531ac39c4f95b82a596312ace9985a78a5c10f
Tushar Verma <tuhsar2022@gmail.com>
2024-01-22 21:16:54 +0530

    added user posts comments models

---
+++ server/src/models/index.js
@@ -0,0 +1,9 @@
+const user = require("./user");
+const posts = require("./posts");
+const comment = require("./comments");
+
+module.exports = {
+  user,
+  posts,
+  comment,
+};
---
+++ server/src/models/posts.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = new mongoose.model("Post", postSchema);
+module.exports = Post;
