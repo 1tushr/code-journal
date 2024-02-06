6bedd49dddeb9309b1f52f18557f16a94c451a7a
Tushar Verma <tuhsar2022@gmail.com>
2024-02-06 18:07:13 +0530

    added cloudinary config

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,6 +1,8 @@
-const cloudinary = require("v2").cloudinary()
+const cloudinary = require("v2").cloudinary();
+const fs = require("fs");
 
-
-
-
-
+cloudinary.cofig({
+  cloud_name: process.env.CLOUDINAY_CLOUD_NAME,
+  api_key: process.env.CLOUDINAY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_KEY,
+});
